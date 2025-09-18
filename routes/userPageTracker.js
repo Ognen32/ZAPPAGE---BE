f5b1726867524550e0b099d42972e7916195af98
Elena <gichevska.elena@uklo.edu.mk>
2025-09-18 14:51:11 +0200

    details

--- routes/userPageTracker.js
+++ routes/userPageTracker.js
@@ -5,8 +5,8 @@ import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.get("/comic/:slug",isAuthenticated,handleUserPages);
-router.post("/comic/:slug/updateCurrentPage",isAuthenticated,handleUpdateUserPage);
+router.get("/comicRead/:slug",isAuthenticated,handleUserPages);
+router.post("/comicRead/:slug/updateCurrentPage",isAuthenticated,handleUpdateUserPage);
 
 
 export default router;
